4830ea918861ef9fcc14e468c7baed2194c1957e
fahad123k <ahmad.nishu08@gmail.com>
2025-02-10 00:48:05 +0530

    add soket timeout db

--- config/db.js
+++ config/db.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
         console.log("⏳ Connecting to MongoDB...");
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds
+            socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+        });
         console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error("❌ MongoDB Connection Failed:", error);
